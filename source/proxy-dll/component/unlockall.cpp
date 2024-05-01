e38ed004e0aefa3dc9ebe89a9b183cb339214f1a
bodnjenie14 <126781031+bodnjenie14@users.noreply.github.com>
2024-05-01 01:52:54 +0100

    sat rank 15 as a test

--- source/proxy-dll/component/unlockall.cpp
+++ source/proxy-dll/component/unlockall.cpp
@@ -2,63 +2,24 @@
 #include "loader/component_loader.hpp"
 #include "definitions/game.hpp"
 #include "definitions/variables.hpp"
+#include "definitions/xassets.hpp"
 #include "command.hpp"
 
 #include <utilities/hook.hpp>
 #include <utilities/json_config.hpp>
 
+namespace game
+{
+	WEAK symbol<int(game::eModes mode, const game::ControllerIndex_t controllerIndex)> LiveStats_GetPrestige{ 0x1438A7390_g };
+	WEAK symbol<int(game::eModes mode, const game::ControllerIndex_t controllerIndex)> LiveStats_GetRank{ 0x1438A7430_g };
+	WEAK symbol<int(const game::ControllerIndex_t controllerIndex, const int statsLocation)> LiveStorage_DoWeHaveStats{ 0x1438B79D0_g };
+}
+
 namespace unlockall
 {
 	namespace
 	{
-		struct unlock_all_s
-		{
-            bool all = false;
-            bool loot = false;
-            bool items = false;
-            bool attachments = false;
-            bool emblems = false;
-            bool challenges = false;
-            bool classes = false;
-        };
-
-        class UnlockController
-        {
-        private:
-            std::unordered_map<std::string, bool unlock_all_s::*> mapping = {
-                {"all", &unlock_all_s::all},
-                {"loot", &unlock_all_s::loot},
-                {"items", &unlock_all_s::items},
-                {"attachments", &unlock_all_s::attachments},
-                {"emblems", &unlock_all_s::emblems},
-                {"challenges", &unlock_all_s::challenges},
-                {"classes", &unlock_all_s::classes}};
-            unlock_all_s unlock;
-
-        public:
-            void changeValue(const std::string& name, bool value)
-            {
-                auto it = mapping.find(name);
-                if (it != mapping.end())
-                {
-                    unlock.*(it->second) = value;
-                }
-            }
-
-            bool getValue(const std::string& name)
-            {
-                auto it = mapping.find(name);
-                if (it != mapping.end())
-                {
-                    return unlock.*(it->second);
-                }
-                return false;
-            }
-        };
-
-		UnlockController unlock;
-
-        // unlock_all_s unlock;
+		std::unordered_map<std::string, bool> unlock;
 
 		utilities::hook::detour loot_getitemquantity_hook;
 		utilities::hook::detour bg_unlockablesisitemattachmentlocked_hook;
@@ -69,6 +30,9 @@ namespace unlockall
 		utilities::hook::detour bg_emblemisentitlementbackgroundgranted_hook;
 		utilities::hook::detour bg_unlockablesgetcustomclasscount_hook;
 		utilities::hook::detour bg_unlockablesisitemlocked_hook;
+
+		utilities::hook::detour livestats_getrank_hook;
+		utilities::hook::detour livestats_getprestige_hook;
 
 		uint32_t zm_loot_table[] =
 		{
@@ -142,12 +106,74 @@ namespace unlockall
 			1000068,			// zm_bgb_secret_shopper
 			1000069 			// zm_bgb_power_vacuum
 		};
+		/*
+				bool is_item_unlocked(int32_t code)
+				{
+					int playerlevel = 18; // TODO. Get player level
+					int readcolumn = 11;
+
+					// read from the csv
+					static game::BO4_AssetRef_t custom_csv = game::AssetRef(fnv1a::generate_hash("project-bo4/internals/ranktable_mp.csv"));
+
+					xassets::stringtable_header* table = xassets::DB_FindXAssetHeader(xassets::ASSET_TYPE_STRINGTABLE, &custom_csv, false, -1).stringtable;
+
+					if (!table || !table->columns_count || table->columns_count < readcolumn)
+					{
+						return false;
+					}
+
+					for (size_t i = 0; i < playerlevel + 1; i++)
+					{
+						auto* rows = &table->values[i * table->columns_count];
+
+						if (rows[0].type != xassets::STC_TYPE_INT || rows[readcolumn].type != xassets::STC_TYPE_STRING) continue;
+
+						if (rows[0].value.int_value <= playerlevel)
+						{
+							if (rows[readcolumn].value.hash_value == code) return true;
+							// if (strstr(rows[readcolumn].value.string_value, code) != nullptr)
+							// {
+							//     return true;
+							// }
+						}
+					}
+					return false;
+				}
+		 */
+
+		void get_player_level()
+		{
+			game::eModes mode = game::Com_SessionMode_GetMode();
+			const auto controller = game::ControllerIndex_t::CONTROLLER_INDEX_FIRST;
+
+			int prestige = game::LiveStats_GetPrestige(mode, controller);
+			int rank = game::LiveStats_GetRank(mode, controller);
+			logger::write(logger::LOG_TYPE_INFO, "player rank(%i) prestige(%i)", rank, prestige);
+
+			if (game::LiveStorage_DoWeHaveStats(controller, 0))
+			{
+				logger::write(logger::LOG_TYPE_INFO, "LiveStorage_DoWeHaveStats True");
+			}
+		}
+
+		int livestats_getrank_stub(game::eModes mode, const game::ControllerIndex_t controllerIndex)
+		{
+			return 15;
+			// return livestats_getrank_hook.invoke<int>(mode, controllerIndex);
+		}
+
+		int livestats_getprestige_stub(game::eModes mode, const game::ControllerIndex_t controllerIndex)
+		{
+			return 2;
+			// return livestats_getprestige_hook.invoke<int>(mode, controllerIndex);
+		}
 
 		inline bool is_zm_loot(int item_id)
 		{
 			auto it = std::find(
 				std::begin(zm_loot_table), std::end(zm_loot_table), item_id);
-			if (it != std::end(zm_loot_table)) {
+			if (it != std::end(zm_loot_table))
+			{
 				return true;
 			}
 			return false;
@@ -155,120 +181,126 @@ namespace unlockall
 
 		int liveinventory_getitemquantity(const game::ControllerIndex_t controllerIndex, const int item_id)
 		{
-			if (unlock.getValue("all") || unlock.getValue("items"))
-            {
+			if (unlock["all"] || unlock["zm_loot"])
+			{
 				int result = is_zm_loot(item_id) ? 999 : 1;
 				return result;
-            }
+			}
 			return loot_getitemquantity_hook.invoke<int>(controllerIndex, item_id);
 		}
 
 		bool bg_unlockedgetchallengeunlockedforindex(game::eModes mode, const game::ControllerIndex_t controllerIndex, uint16_t index, int32_t itemIndex)
 		{
-            if (unlock.getValue("all") || unlock.getValue("challenges"))
-            {
-                return true;
-            }
+			if (unlock["all"] || unlock["challenges"])
+			{
+				return true;
+			}
 			bg_unlockedgetchallengeunlockedforindex_hook.invoke<bool>(mode, controllerIndex, index, itemIndex);
 		}
 
 		bool bg_unlockablesitemoptionlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int itemIndex, int32_t optionIndex)
 		{
-            if (unlock.getValue("all") || unlock.getValue("items"))
-            {
-                return false;
-            }
+			if (unlock["all"] || unlock["itemoptions"])
+			{
+				return false;
+			}
 			return bg_unlockablesitemoptionlocked_hook.invoke<bool>(mode, controllerIndex, itemIndex, optionIndex);
 		}
 
-        bool bg_unlockablesisitemattachmentlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int32_t itemIndex, int32_t attachmentNum)
-        {
-            if (unlock.getValue("all") || unlock.getValue("attachments"))
-            {
-                return false;
-            }
-            return bg_unlockablesisitemattachmentlocked_hook.invoke<bool>(mode, controllerIndex, itemIndex, attachmentNum);
-        }
-
-        bool bg_unlockablesisattachmentslotlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int32_t itemIndex, int32_t attachmentSlotIndex)
+		bool bg_unlockablesisitemattachmentlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int32_t itemIndex, int32_t attachmentNum)
 		{
-			if (unlock.getValue("all") || unlock.getValue("attachments"))
-            {
-                return false;
-            }
+			if (unlock["all"] || unlock["attachments"])
+			{
+				return false;
+			}
+			return bg_unlockablesisitemattachmentlocked_hook.invoke<bool>(mode, controllerIndex, itemIndex, attachmentNum);
+		}
+
+		bool bg_unlockablesisattachmentslotlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int32_t itemIndex, int32_t attachmentSlotIndex)
+		{
+			if (unlock["all"] || unlock["attachmentslot"])
+			{
+				return false;
+			}
 			return bg_unlockablesisattachmentslotlocked_hook.invoke<bool>(mode, controllerIndex, itemIndex, attachmentSlotIndex);
 		}
 
 		bool bg_unlockablesemblemorbackinglockedbychallenge(game::eModes mode, const game::ControllerIndex_t controllerIndex, void* challengeLookup, bool otherPlayer)
 		{
-			if (unlock.getValue("all") || unlock.getValue("emblems"))
-            {
-                return false;
-            }
+			if (unlock["all"] || unlock["emblems"])
+			{
+				return false;
+			}
 			return bg_unlockablesemblemorbackinglockedbychallenge_hook.invoke<bool>(mode, controllerIndex, challengeLookup, otherPlayer);
 		}
 
 		bool bg_emblemisentitlementbackgroundgranted(const game::ControllerIndex_t controllerIndex, uint16_t backgroundId)
 		{
-			if (unlock.getValue("all") || unlock.getValue("emblems"))
-            {
-                return true;
-            }
+			if (unlock["all"] || unlock["backgrounds"])
+			{
+				return true;
+			}
 			return bg_emblemisentitlementbackgroundgranted_hook.invoke<bool>(controllerIndex, backgroundId);
 		}
 
 		bool bg_unlockablesisitemlocked(game::eModes mode, const game::ControllerIndex_t controllerIndex, int32_t itemIndex)
 		{
-			if (unlock.getValue("all") || unlock.getValue("items"))
-            {
-                return false;
-            }
+			if (unlock["all"] || unlock["items"])
+			{
+				return false;
+				// return is_item_unlocked(itemIndex);
+			}
 			return bg_unlockablesisitemlocked_hook.invoke<bool>(mode, controllerIndex, itemIndex);
 
 		}
 
 		int32_t bg_unlockablesgetcustomclasscount(game::eModes mode, const game::ControllerIndex_t controllerIndex)
 		{
-			if (unlock.getValue("all") || unlock.getValue("classes"))
-            {
-                return 12; // max 12
-            }
+			if (unlock["all"] || unlock["classes"])
+			{
+				return 12; // max 12
+			}
 			return bg_unlockablesgetcustomclasscount_hook.invoke<int>(mode, controllerIndex);
 		}
 
 		void unlock_func(const command::params& params)
-        {
-            if (params.size() < 2) return;
+		{
+			if (params.size() < 2) return;
 
-            std::string arg1 = params[1];
+			std::string arg1 = params[1];
 
-            if (params.size() == 3)
-            {
-                std::string arg2 = params[2];
-                bool TF = (arg2 == "true" || arg2 == "True" || arg2 == "1");
-                unlock.changeValue(arg1, TF);
-            }
-            else
-            {
-                unlock.changeValue(arg1, true);
-            }
-        }
+			if (params.size() == 3)
+			{
+				std::string arg2 = params[2];
+				bool TF = (arg2 == "true" || arg2 == "True" || arg2 == "1");
+				unlock[arg1] = TF;
+			}
+			else
+			{
+				unlock[arg1] = true;
+			}
+		}
 	}
 
-    class component final : public component_interface
+	class component final : public component_interface
 	{
 	public:
 		void post_unpack() override
 		{
-			command::add("unlock", [&](const command::params& params){unlock_func(params);});
-			unlock.changeValue("all", utilities::json_config::ReadBoolean("unlock", "all", false));
-			unlock.changeValue("loot", utilities::json_config::ReadBoolean("unlock", "loot", false));
-			unlock.changeValue("items", utilities::json_config::ReadBoolean("unlock", "items", false));
-			unlock.changeValue("attachments", utilities::json_config::ReadBoolean("unlock", "attachments", false));
-			unlock.changeValue("emblems", utilities::json_config::ReadBoolean("unlock", "emblems", false));
-			unlock.changeValue("challenges", utilities::json_config::ReadBoolean("unlock", "challenges", false));
-			unlock.changeValue("classes", utilities::json_config::ReadBoolean("unlock", "classes", false));
-			
+			command::add("get_player_level", get_player_level);
+
+			command::add("unlock", [&](const command::params& params) {unlock_func(params); });
+			unlock["all"] = utilities::json_config::ReadBoolean("unlock", "all", false);
+			unlock["attachments"] = utilities::json_config::ReadBoolean("unlock", "attachments", false);
+			unlock["attachmentslot"] = utilities::json_config::ReadBoolean("unlock", "attachmentslot", false);
+			unlock["backgrounds"] = utilities::json_config::ReadBoolean("unlock", "backgrounds", false);
+			unlock["challenges"] = utilities::json_config::ReadBoolean("unlock", "challenges", false);
+			unlock["classes"] = utilities::json_config::ReadBoolean("unlock", "classes", false);
+			unlock["emblems"] = utilities::json_config::ReadBoolean("unlock", "emblems", false);
+			unlock["itemoptions"] = utilities::json_config::ReadBoolean("unlock", "itemoptions", false);
+			unlock["items"] = utilities::json_config::ReadBoolean("unlock", "items", false);
+			unlock["zm_loot"] = utilities::json_config::ReadBoolean("unlock", "zm_loot", false);
+
 			loot_getitemquantity_hook.create(0x1437F6ED0_g, liveinventory_getitemquantity);
 			bg_unlockablesisitemattachmentlocked_hook.create(0x1406B34D0_g, bg_unlockablesisitemattachmentlocked);
 			bg_unlockablesisattachmentslotlocked_hook.create(0x1406B3290_g, bg_unlockablesisattachmentslotlocked);
@@ -278,8 +310,11 @@ namespace unlockall
 			bg_emblemisentitlementbackgroundgranted_hook.create(0x144184D20_g, bg_emblemisentitlementbackgroundgranted);
 			bg_unlockablesgetcustomclasscount_hook.create(0x1406ae060_g, bg_unlockablesgetcustomclasscount);
 			bg_unlockablesisitemlocked_hook.create(0x1406B3AA0_g, bg_unlockablesisitemlocked);
-        }
-    };
+
+			livestats_getrank_hook.create(0x1438A7430_g, livestats_getrank_stub);
+			livestats_getprestige_hook.create(0x1438A7390_g, livestats_getprestige_stub);
+		}
+	};
 }
 
 REGISTER_COMPONENT(unlockall::component)
