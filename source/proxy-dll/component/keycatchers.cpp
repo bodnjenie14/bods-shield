a4f7dd68694102b7f84ae38cd622e19ca6009cc8
bodnjenie14 <126781031+bodnjenie14@users.noreply.github.com>
2024-09-06 00:23:23 +0100

    Update keycatchers.cpp

--- source/proxy-dll/component/keycatchers.cpp
+++ source/proxy-dll/component/keycatchers.cpp
@@ -4,68 +4,79 @@
 
 #include <utilities/hook.hpp>
 #include <utilities/concurrency.hpp>
+#include <algorithm>
 
 namespace keycatchers
 {
-	namespace
-	{
-		utilities::concurrency::container<std::vector<ke_typedef>, std::recursive_mutex> key_event_callbacks;
-		utilities::concurrency::container<std::vector<ch_typedef>, std::recursive_mutex> char_event_callbacks;
+    namespace
+    {
+        const std::vector<int> excluded_keys = { 
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 27
+        };
 
-		utilities::hook::detour cl_key_event_hook;
-		void cl_key_event_stub(const int localClientNum, const int key, bool down, const unsigned int time)
-		{
-			key_event_callbacks.access([&](std::vector<ke_typedef>& callbacks)
-				{
-					for (auto& func : callbacks) {
-						if (!func(localClientNum, key, down, time))
-							return;
-					}
-				});
+        utilities::concurrency::container<std::vector<ke_typedef>, std::recursive_mutex> key_event_callbacks;
+        utilities::concurrency::container<std::vector<ch_typedef>, std::recursive_mutex> char_event_callbacks;
 
-			cl_key_event_hook.invoke<void>(localClientNum, key, down, time);
-		}
+        utilities::hook::detour cl_key_event_hook;
+        void cl_key_event_stub(const int localClientNum, const int key, bool down, const unsigned int time)
+        {
+            if (down && std::find(excluded_keys.begin(), excluded_keys.end(), key) == excluded_keys.end())
+            {
+                key_event_callbacks.access([&](std::vector<ke_typedef>& callbacks)
+                {
+                    for (auto& func : callbacks) {
+                        if (!func(localClientNum, key, down, time))
+                            return;
+                    }
+                });
+            }
 
-		utilities::hook::detour cl_char_event_hook;
-		void cl_char_event_stub(const int localClientNum, const int key, bool isRepeated)
-		{
-			char_event_callbacks.access([&](std::vector<ch_typedef>& callbacks)
-				{
-					for (auto& func : callbacks) {
-						if (!func(localClientNum, key, isRepeated))
-							return;
-					}
-				});
+            cl_key_event_hook.invoke<void>(localClientNum, key, down, time);
+        }
 
-			cl_char_event_hook.invoke<void>(localClientNum, key, isRepeated);
-		}
-	}
+        utilities::hook::detour cl_char_event_hook;
+        void cl_char_event_stub(const int localClientNum, const int key, bool isRepeated)
+        {
+            if (std::find(excluded_keys.begin(), excluded_keys.end(), key) == excluded_keys.end())
+            {
+                char_event_callbacks.access([&](std::vector<ch_typedef>& callbacks)
+                {
+                    for (auto& func : callbacks) {
+                        if (!func(localClientNum, key, isRepeated))
+                            return;
+                    }
+                });
+            }
 
-	void add_key_event(ke_typedef&& func)
-	{
-		key_event_callbacks.access([&func](std::vector<ke_typedef>& callbacks)
-			{
-				callbacks.push_back(std::move(func));
-			});
-	}
+            cl_char_event_hook.invoke<void>(localClientNum, key, isRepeated);
+        }
+    }
 
-	void add_char_event(ch_typedef&& func)
-	{
-		char_event_callbacks.access([&func](std::vector<ch_typedef>& callbacks)
-			{
-				callbacks.push_back(std::move(func));
-			});
-	}
+    void add_key_event(ke_typedef&& func)
+    {
+        key_event_callbacks.access([&func](std::vector<ke_typedef>& callbacks)
+        {
+            callbacks.push_back(std::move(func));
+        });
+    }
 
-	class component final : public component_interface
-	{
-	public:
-		void post_unpack() override
-		{
-			cl_key_event_hook.create(0x142839250_g, cl_key_event_stub);
-			cl_char_event_hook.create(0x142836F80_g, cl_char_event_stub);
-		}
-	};
+    void add_char_event(ch_typedef&& func)
+    {
+        char_event_callbacks.access([&func](std::vector<ch_typedef>& callbacks)
+        {
+            callbacks.push_back(std::move(func));
+        });
+    }
+
+    class component final : public component_interface
+    {
+    public:
+        void post_unpack() override
+        {
+            cl_key_event_hook.create(0x142839250_g, cl_key_event_stub);
+            cl_char_event_hook.create(0x142836F80_g, cl_char_event_stub);
+        }
+    };
 }
 
 REGISTER_COMPONENT(keycatchers::component)
